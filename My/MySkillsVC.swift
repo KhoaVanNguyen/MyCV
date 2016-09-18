37ace0b3bf73f6e267bb3a0092b0b7117579ca5d
Khoa Nguyen <vankhoa0603@gmail.com>
2016-09-18 14:39:51 +0700

    Finish tabbar icon

---
+++ My/MySkillsVC.swift
@@ -0,0 +1,35 @@
+//
+//  MySkillsVC.swift
+//  MyCV
+//
+//  Created by Khoa on 9/18/16.
+//  Copyright © 2016 Khoa. All rights reserved.
+//
+
+import UIKit
+
+class MySkillsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

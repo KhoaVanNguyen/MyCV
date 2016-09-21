830655db198279779279471fad76c4d9395f982d
Khoa Nguyen <vankhoa0603@gmail.com>
2016-09-21 10:42:32 +0700

    Finish StatView with Inspectable fields

--- My/StatView.swift
+++ My/StatView.swift
@@ -10,29 +10,54 @@ import UIKit
 
 @IBDesignable
 class StatView: UIView {
+    // MARK:- Properties and IBInspectable
     let percentLabel  = UILabel()
     let skillLable = UILabel()
     
-    var range : CGFloat = 10
-    var curValue : CGFloat = 0{
-        didSet{
-            configure()
-        }
-    }
-    let margin: CGFloat = 10
-    
     let bgLayer = CAShapeLayer()
-    @IBInspectable var bgColor : UIColor = UIColor.gray{
+    @IBInspectable var BGColor : UIColor = UIColor.gray{
         didSet{
             configure()
         }
     }
     let fgLayer = CAShapeLayer()
-    @IBInspectable var fgColor : UIColor = UIColor.black{
+    @IBInspectable var FGColor : UIColor = UIColor.black{
         didSet{
             configure()
         }
     }
+    @IBInspectable var SkillName : String = "Programming"{
+        didSet{
+            configure()
+        }
+    }
+    @IBInspectable var PercentMargin : CGFloat = 0{
+        didSet{
+            configure()
+        }
+    }
+    @IBInspectable var SkillNameMarginX : CGFloat = 0{
+        didSet{
+            configure()
+        }
+    }
+    @IBInspectable var SkillNameMarginY : CGFloat = 20{
+        didSet{
+            configure()
+        }
+    }
+    @IBInspectable var Total : CGFloat = 100{
+        didSet{
+            configure()
+        }
+    }
+    @IBInspectable var CurrentValue : CGFloat = 100{
+        didSet{
+            configure()
+        }
+    }
+    
+    // MARK:- Awake and prepare
     override func awakeFromNib() {
         super.awakeFromNib()
         setup()
@@ -43,6 +68,8 @@ class StatView: UIView {
         setup()
         configure()
     }
+    
+    // MARK:- Setup
     func setup(){
         // setup background layer
         bgLayer.lineWidth = 20
@@ -53,36 +80,35 @@ class StatView: UIView {
         // setup background layer
         fgLayer.lineWidth = 20
         fgLayer.fillColor = nil
-        fgLayer.strokeEnd = 0.5
+        editPercent(range: Total, curValue: CurrentValue)
         layer.addSublayer(fgLayer)
         // setup percent label
-        percentLabel.font = UIFont.systemFont(ofSize: 22)
+        percentLabel.font = UIFont.systemFont(ofSize: 12)
         percentLabel.textColor = UIColor.white
-        percentLabel.text = "0/12"
+        editPercent(range: Total, curValue: CurrentValue)
         percentLabel.translatesAutoresizingMaskIntoConstraints = false
         addSubview(percentLabel)
         // setup skill name
-        skillLable.font = UIFont.systemFont(ofSize: 18)
-        skillLable.text = "Programming"
+        skillLable.font = UIFont.systemFont(ofSize: 12)
+        editSkillName(skillName: SkillName)
         skillLable.textColor = UIColor.white
         skillLable.translatesAutoresizingMaskIntoConstraints = false
         addSubview(skillLable)
         
-        // setup constraints
-        let percentLabelCenterX = percentLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor)
-        let percentLabelCenterY = percentLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -margin)
-        
-        NSLayoutConstraint.activate([percentLabelCenterX, percentLabelCenterY])
-        
-        let skillLabelCenterX = skillLable.centerXAnchor.constraint(equalTo: self.centerXAnchor, constant: -margin)
-        let skillLabelCenterY = skillLable.centerYAnchor.constraint(equalTo: self.bottomAnchor, constant: -margin)
-        NSLayoutConstraint.activate([skillLabelCenterX, skillLabelCenterY])
-    
+        // setup constraints 
+        setupConstraints(PercentMargin,SkillNameMarginX,SkillNameMarginY)
     }
     func configure(){
-        percentLabel.text = String(format: "%.0f/%.0f", curValue,range)
-        bgLayer.strokeColor = bgColor.cgColor
-        fgLayer.strokeColor = fgColor.cgColor
+        
+        setupConstraints(PercentMargin,SkillNameMarginX,SkillNameMarginY)
+        editSkillName(skillName: SkillName)
+        editPercent(range: Total, curValue: CurrentValue)
+      
+        let percent = ( CurrentValue/Total ) * 100
+        let percentString = String(format: "%.0f", percent)
+        percentLabel.text = "\(percentString)%"
+        bgLayer.strokeColor = BGColor.cgColor
+        fgLayer.strokeColor = FGColor.cgColor
     }
     override func layoutSubviews() {
         super.layoutSubviews()
@@ -98,6 +124,25 @@ class StatView: UIView {
         let radius = self.bounds.width * 0.35
         let path = UIBezierPath(arcCenter: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: true)
         shapeLayer.path = path.cgPath
+    }
+    
+    func setupConstraints(_ percentMargin: CGFloat, _ skillNameMarginX: CGFloat, _ skillNameMarginY: CGFloat){
+        // setup constraints
+        let percentLabelCenterX = percentLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor)
+        let percentLabelCenterY = percentLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -percentMargin)
+        
+        NSLayoutConstraint.activate([percentLabelCenterX, percentLabelCenterY])
+        
+        let skillLabelCenterX = skillLable.centerXAnchor.constraint(equalTo: self.centerXAnchor, constant: -skillNameMarginX)
+        let skillLabelCenterY = skillLable.centerYAnchor.constraint(equalTo: self.bottomAnchor, constant: -skillNameMarginY)
+        NSLayoutConstraint.activate([skillLabelCenterX, skillLabelCenterY])
+    }
+    func editSkillName(skillName : String){
+        skillLable.text = skillName
+    }
+    func editPercent(range : CGFloat, curValue : CGFloat ){
+        fgLayer.strokeEnd = CurrentValue/Total
+        percentLabel.text = "\(CurrentValue)/\(Total)"
     }
 }
 

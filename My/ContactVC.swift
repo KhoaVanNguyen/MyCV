a8e28923e6368bf155c8306384361e3094b7bad6
Khoa Nguyen <vankhoa0603@gmail.com>
2016-09-22 11:12:50 +0700

    Add sending email, calling Skype

--- My/ContactVC.swift
+++ My/ContactVC.swift
@@ -7,7 +7,8 @@
 //
 
 import UIKit
-class ContactVC: UIViewController {
+import MessageUI
+class ContactVC: UIViewController, MFMailComposeViewControllerDelegate  {
 
 
     override func viewDidLoad() {
@@ -16,6 +17,24 @@ class ContactVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func skypeBtnPressed(_ sender: AnyObject) {
+        let installed = UIApplication.shared.canOpenURL(URL(string: "skype:")!)
+        if installed {
+            UIApplication.shared.openURL(URL(string: "skype:skype:vankhoa117?call")!)
+            
+        } else {
+            
+            UIApplication.shared.openURL(URL(string: "https://itunes.apple.com/in/app/skype/id304878510?mt=8")!)
+        }
+    }
+    @IBAction func emailBtnPressed(_ sender: AnyObject) {
+        let MailVC = configureEmail()
+        if MFMailComposeViewController.canSendMail(){
+            present(MailVC, animated: true, completion: nil)
+        }else{
+            showEmailAlert()
+        }
+    }
     @IBAction func callBtnPressed(_ sender: AnyObject) {
         let alert = UIAlertController(title: "Call Khoa", message: "Do you want to call 01647778186 (Khoa)", preferredStyle: .alert)
         
@@ -28,6 +47,9 @@ class ContactVC: UIViewController {
         
         present(alert, animated: true, completion: nil)
     }
+    
+    
+    //MARK:- Functions
     func callNumber(){
         if let url = URL(string: "tel://\(01647778186)") {
             UIApplication.shared.openURL(url)
@@ -38,4 +60,43 @@ class ContactVC: UIViewController {
         let url = URL(string: "mailto:\(email)")
         UIApplication.shared.openURL(url!)
     }
+    
+    func configureEmail() -> MFMailComposeViewController{
+        let composeMailVC = MFMailComposeViewController()
+        composeMailVC.setToRecipients(["vankhoa0603@gmail.com", "khoa@niviki.com"])
+        composeMailVC.setSubject("Send From Khoa's CV App")
+        composeMailVC.setMessageBody("\n Hello Khoa, \n Say something to Khoa", isHTML: false)
+        composeMailVC.mailComposeDelegate = self
+        return composeMailVC
+    }
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        switch result.rawValue {
+        case MFMailComposeResult.cancelled.rawValue:
+            print("Mail canceled")
+        case MFMailComposeResult.failed.rawValue:
+            print("Mail failed")
+        case MFMailComposeResult.saved.rawValue:
+            print("Mail saved")
+        case MFMailComposeResult.sent.rawValue:
+            print("Mail sent")
+        default:
+            print("Unknow error")
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    func showEmailAlert(){
+        let alert = UIAlertController(title: "Mail Setup Error", message: "You don't setup email account for Iphone", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Setup Email Account", style: .default) { (action) in
+            let settingsUrl = URL(string: UIApplicationOpenSettingsURLString)
+            if let url = settingsUrl {
+                UIApplication.shared.openURL(url)
+            }
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+
 }

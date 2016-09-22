59343ba88d2d6b7c52e091cf796fb86c265cb19f
Khoa Nguyen <vankhoa0603@gmail.com>
2016-09-22 10:02:38 +0700

    Repair the Contact screen ver 2 - simpler

--- My/ContactVC.swift
+++ My/ContactVC.swift
@@ -16,4 +16,26 @@ class ContactVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func callBtnPressed(_ sender: AnyObject) {
+        let alert = UIAlertController(title: "Call Khoa", message: "Do you want to call 01647778186 (Khoa)", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "Call Now", style: .default) { (action) in
+            self.callNumber()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    func callNumber(){
+        if let url = URL(string: "tel://\(01647778186)") {
+            UIApplication.shared.openURL(url)
+        }
+    }
+    func sendEmail(){
+        let email = "vankhoa0603@gmail.com"
+        let url = URL(string: "mailto:\(email)")
+        UIApplication.shared.openURL(url!)
+    }
 }
